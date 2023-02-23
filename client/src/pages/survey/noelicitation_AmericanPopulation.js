54746dce40be90ee6c459d89a03242b3ef14d52a
subham27-07 <subham93.sah@gmail.com>
2023-02-23 15:34:40 -0500

    no elicitation cleaned

--- client/src/pages/survey/noelicitation_AmericanPopulation.js
+++ client/src/pages/survey/noelicitation_AmericanPopulation.js
@@ -60,32 +60,10 @@ const Noelicitation_AmericanPopulation = (props) => {
   defaultThemeColors["$header-background-color"] = "#4a4a4a";
   defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
 
-  const correctStr = "Correct";
-  const inCorrectStr = "Incorrect";
 
   Survey.StylesManager.applyTheme();
 
-  const onCompleting = (survey, options) => {
-    // console.log(options);
-    let allTrue = true;
-    survey.getAllQuestions().forEach((q) => {
-      let correct = isAnswerCorrect(q);
-      correct = correct == undefined ? true : correct;
-
-      allTrue = allTrue && correct;
-      renderCorrectAnswer(q);
-    });
-    quizResponses.current.push(survey.data);
-    if (allTrue) {
-      options.allowComplete = true;
-    } else {
-      options.allowComplete = false;
-    }
-  };
-
   const onComplete = (survey, options) => {
-    //Write survey results into database
-    // console.log(options);
 
     console.log("Survey results: " + JSON.stringify(quizResponses.current));
     axios.post("/api/noelicitation_AmericanPopulation", quizResponses.current).then((response) => {
@@ -94,72 +72,14 @@ const Noelicitation_AmericanPopulation = (props) => {
     });
   };
 
-  const onCurrentPageChanging = (survey, option) => {
-    if (!option.isNextPage) return;
-    let allTrue = false;
-    survey.getAllQuestions().forEach((q) => {
-      if (survey.currentPage == q.page) {
-        let correct = isAnswerCorrect(q);
-        correct = correct == undefined ? true : correct;
 
-        allTrue = allTrue && correct;
-        renderCorrectAnswer(q);
-      }
-    });
-    console.log(allTrue);
-  };
-
-  function getTextHtml(text, str, isCorrect) {
-    if (text.indexOf(str) < 0) return undefined;
-    return (
-      text.substring(0, text.indexOf(str)) +
-      "<span style='color:" +
-      (isCorrect ? "green" : "red") +
-      "'>" +
-      str +
-      "</span>"
-    );
-  }
-  function isAnswerCorrect(q) {
-    const right = q.correctAnswer;
-    if (right == undefined) return undefined;
-    if (!right || q.isEmpty()) return undefined;
-    var left = q.value;
-    if (!Array.isArray(right)) return right == left;
-    if (!Array.isArray(left)) left = [left];
-    for (var i = 0; i < left.length; i++) {
-      if (right.indexOf(left[i]) < 0) return false;
-    }
-    return true;
-  }
-
-  function renderCorrectAnswer(q) {
-    if (!q) return;
-    const isCorrect = isAnswerCorrect(q);
-    if (!q.prevTitle) {
-      q.prevTitle = q.title;
-    }
-    if (isCorrect === undefined) {
-      q.title = q.prevTitle;
-    } else {
-      q.title = q.prevTitle + " " + (isCorrect ? correctStr : inCorrectStr);
-    }
-  }
+ 
 
   const model = new Survey.Model(json);
   model.showCompletedPage = false;
   model.questionTitleTemplate = "";
   model.showQuestionNumbers = "none";
-  model.onTextMarkdown.add((sender, options) => {
-    var text = options.text;
-    var html = getTextHtml(text, correctStr, true);
-    if (!html) {
-      html = getTextHtml(text, inCorrectStr, false);
-    }
-    if (!!html) {
-      options.html = html;
-    }
-  });
+ 
 
   return (
     <Container
@@ -181,7 +101,7 @@ const Noelicitation_AmericanPopulation = (props) => {
         }}
       >
         <Typography variant="h3">
-          How Bad Is the   <span style={{ fontWeight: "bold" }}> Drug Overdose... </span> Epidemic?   
+          How Bad Is the   <span style={{ fontWeight: "bold" }}> Drug Overdose</span> Epidemic?   
         </Typography>
         <Divider></Divider>
         
@@ -190,8 +110,7 @@ const Noelicitation_AmericanPopulation = (props) => {
       <Survey.Survey
         model={model}
         onComplete={onComplete}
-        onCompleting={onCompleting}
-        onCurrentPageChanging={onCurrentPageChanging}
+     
       />
     </Container>
   );
