3e466b9bc06dea5f7256249994d0cd99752ec91b
Milad Rogha <milad.rogha@gmail.com>
2023-02-24 19:00:05 -0500

    change to article look for textelicitation

--- client/src/pages/survey/textelicitation_Opioids.js
+++ client/src/pages/survey/textelicitation_Opioids.js
@@ -131,7 +131,47 @@ const Textelicitation_Opioids = (props) => {
             "</span>"
         );
     }
+const surveyCompleted = ()=>{
+        return (    <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingTop: "30px",
+            }}
+        >
+            <Typography variant="h5">{message}</Typography>
+            <img src={"https://raw.githubusercontent.com/subham27-07/youdrawitnew/main/d.JPG"} width="100%"
+                 height="100%" alt="Completion image"/>
+            <p className={`${styles.paragraph} ${styles.textBody} ${styles.txtNormal}`}>Since 2002, the <span
+                className={styles.txtImportant}>number</span> of Americans who have died every year from overdoses of <span
+                className={styles.txtImportant}>synthetic opioids</span> has increased by more
+                than <span
+                    style={{fontWeight: "bold"}}>5451 percent</span>. Substance use disorders refers to direct
+                deaths from
+                overdoses of <span style={{fontWeight: "bold"}}>illicit drugs synthetic opioids (mostly fentanyl)</span> .“We
+                know that substance use is more dangerous than it has ever been, as fentanyl has continued to
+                permeate the illicit drug supply, increasing the risk for overdoses among both people with
+                substance use disorders as well as those who use drugs occasionally,” said Dr. Nora Volkow,
+                director of the National Institute on Drug Abuse. Deaths involving synthetic opioids such as
+                fentanyl increased by a marked 18% in 2021, according to the CDC data. Deaths involving cocaine
+                and psychostimulants such as methamphetamine were also significantly more frequent, while those
+                involving heroin decreased.
+            </p>
 
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                    let nextPage = pageHandler(location.pathname);
+                    history.push(nextPage);
+                }}
+            >
+                Continue
+            </Button>
+        </div>)
+}
     function isAnswerCorrect(q) {
         const right = q.correctAnswer;
         if (right == undefined) return undefined;
@@ -173,83 +213,49 @@ const Textelicitation_Opioids = (props) => {
 
 
     return (
-        <Container
-            maxWidth={false}
+<div>
+    <Container
+        maxWidth={false}
+        style={{
+            width: "100%",
+            overflow: "auto",
+            height: "100%",
+            paddingTop: "30px",
+            paddingBottm: "30px",
+        }}
+    >
+        <div
             style={{
-                width: "100%",
-                overflow: "auto",
-                height: "100%",
-                paddingTop: "30px",
-                paddingBottm: "30px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
             }}
         >
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Typography variant="h3">
+            <Typography variant="h3">
                             <span className={`${styles.textBody} ${styles.title}`}>How Bad Is the <span
                                 style={{fontWeight: "bold"}}> Drug Overdose </span> epidemic?</span>
-                </Typography>
-                <Typography variant={"body1"}>
+            </Typography>
+            <Typography variant={"body1"}>
                    <span
                        className={styles.txtImportantUnique}>Article 3</span><p> Since 2002, the <span
-                        className={styles.txtImportant}>number</span> of Americans who have died every year from overdoses of <span
-                    className={styles.txtImportant}>synthetic opioids _______.</span></p>
-                </Typography>
+                className={styles.txtImportant}>number</span> of Americans who have died every year from overdoses of <span
+                className={styles.txtImportant}>synthetic opioids _______.</span></p>
+            </Typography>
 
-            </div>
+        </div>
 
-            <Survey.Survey
-                model={model}
-                onComplete={onComplete}
-                onCompleting={onCompleting}
-                onCurrentPageChanging={onCurrentPageChanging}
-            />
-            {completed ? (
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        paddingTop: "30px",
-                    }}
-                >
-                    <Typography variant="h5">{message}</Typography>
-                    <img src={"https://raw.githubusercontent.com/subham27-07/youdrawitnew/main/d.JPG"} width="60%"
-                         height="100%" alt="Completion image"/>
-                    <p className={`${styles.paragraph} ${styles.textBody} ${styles.txtNormal}`}>...has increased by more
-                        than <span
-                            style={{fontWeight: "bold"}}>5451 percent</span>. Substance use disorders refers to direct
-                        deaths from
-                        overdoses of <span style={{fontWeight: "bold"}}>illicit drugs synthetic opioids (mostly fentanyl)</span> .“We
-                        know that substance use is more dangerous than it has ever been, as fentanyl has continued to
-                        permeate the illicit drug supply, increasing the risk for overdoses among both people with
-                        substance use disorders as well as those who use drugs occasionally,” said Dr. Nora Volkow,
-                        director of the National Institute on Drug Abuse. Deaths involving synthetic opioids such as
-                        fentanyl increased by a marked 18% in 2021, according to the CDC data. Deaths involving cocaine
-                        and psychostimulants such as methamphetamine were also significantly more frequent, while those
-                        involving heroin decreased.
-                    </p>
-
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => {
-                            let nextPage = pageHandler(location.pathname);
-                            history.push(nextPage);
-                        }}
-                    >
-                        Continue
-                    </Button>
-                </div>
-            ) : null}
-        </Container>
+        <Survey.Survey
+            model={model}
+            onComplete={onComplete}
+            onCompleting={onCompleting}
+            onCurrentPageChanging={onCurrentPageChanging}
+        />
+    </Container>
+    {completed ? (
+        surveyCompleted()
+    ) : null}
+</div>
     );
 };
 
